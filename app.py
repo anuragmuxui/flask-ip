b78cec95983cf75ea94ec9ba3fa6ab0022592044
ANURAG M <56573388+anuragmuxui@users.noreply.github.com>
2025-01-26 01:02:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, jsonify, request
+import requests
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_ip_info')
+def get_ip_info():
+    try:
+        # Fetch the public IP address of the client
+        ip_response = requests.get('https://api.ipify.org?format=json')
+        user_ip = ip_response.json().get('ip')
+        
+        # Fetch IP information from ipwho.is
+        response = requests.get(f'http://ipwho.is/{user_ip}?output=json')
+        data = response.json()
+        return jsonify({
+            'ip': data.get('ip'),
+            'isp': data.get('connection', {}).get('isp'),
+            'city': data.get('city'),
+            'region': data.get('region'),
+            'country': data.get('country')
+        })
+    except Exception as e:
+        return jsonify({'error': 'Unable to fetch IP information'}), 500
+if __name__ == "__main__":
+    app.run(debug=False)
